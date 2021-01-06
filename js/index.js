0275301ff290e8c45b9fd7693cb3007387c4917c
gobltpqls99 <gobltpqls99@naver.com>
2021-01-06 22:08:57 +0900

    ..

--- js/index.js
+++ js/index.js
@@ -1,21 +1,45 @@
 
-   var swiper = new Swiper('.swiper-container', {
-	loop: true,
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-   });
 
 
-   var swiper = new Swiper('product-wrapper > .swiper-container', {
-	slidesPerView: 3,
-	spaceBetween: 30,
-	slidesPerGroup: 3,
-	loop: true,
-	loopFillGroupWithBlank: true,
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-   });
+
+
+ 
+function mainBanner() {
+	 var swiper = new Swiper('.main-wrapper.swiper-container', {
+		loop: true,
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev',
+		},
+	   });
+
+ }  
+function Product() {
+	 var swiper = new Swiper('.product-wrap.swiper-container', {
+	   slidesPerView: 4,
+	   slidesPerGroup: 4,
+	   loop: true,
+	   loopFillGroupWithBlank: true,
+	   pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	   },
+	   navigation: {
+		nextEl: '.bt-next',
+		prevEl: '.bt-prev',
+	   },
+	 });
+ }
+
+
+
+Product ();
+mainBanner();
+
+
+
+
+
+
+
+
